0ace9be808fe6f2ad0d6586da7197571d238081d
bartekw2213 <bartekw2213@gmail.com>
2021-08-17 11:47:33 +0200

    Used SDL_Window_Wrapper class in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "SDL_Objects.hpp"
 
 int main(int argc, char* argv[]) {
+    SDL_Window_Wrapper window_wrapper;
+
     SDL_Manager::initialize();
+
+    window_wrapper.initialize_window("Simple Game", 400, 400);
+
     SDL_Manager::quit();
     return 0;
 }
