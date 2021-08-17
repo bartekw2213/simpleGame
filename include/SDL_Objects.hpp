9369a883514eb2db591235057b11bbff1c493649
bartekw2213 <bartekw2213@gmail.com>
2021-08-17 11:50:51 +0200

    Added method to SDL_Manager that throws a runtime error when needed

--- include/SDL_Objects.hpp
+++ include/SDL_Objects.hpp
@@ -6,9 +6,10 @@
 class SDL_Manager {
    public:
     static void initialize() {
-        if (SDL_Init(SDL_INIT_VIDEO) != 0) throw std::runtime_error(SDL_GetError());
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) SDL_Manager::throw_SDL_error();
     };
     static void quit() { SDL_Quit(); };
+    static void throw_SDL_error() { throw std::runtime_error(SDL_GetError()); }
 };
 
 class SDL_Window_Wrapper {
