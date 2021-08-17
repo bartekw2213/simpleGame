4a976415fc421ee50e4f0e2a5ec434c83cb6e573
bartekw2213 <bartekw2213@gmail.com>
2021-08-17 10:21:46 +0200

    Added SDL initialization and closing with use of SDL_Manager class

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,7 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
+#include "SDL_Objects.hpp"
 
 int main(int argc, char* argv[]) {
-    // retutns zero on success else non-zero
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
-    }
-    SDL_Window* win = SDL_CreateWindow("GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
-    while (1)
-        ;
-
+    SDL_Manager::initialize();
+    SDL_Manager::quit();
     return 0;
 }
