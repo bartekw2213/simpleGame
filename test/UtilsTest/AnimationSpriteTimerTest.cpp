0b1d1a2755b0743240daf2a3897c7647eeca403c
bartekw2213 <bartekw2213@gmail.com>
2021-08-18 12:46:20 +0200

    Created initial setup for testing Utils files

---
+++ test/UtilsTest/AnimationSpriteTimerTest.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "SDL_Objects.hpp"
+#include "Utils.hpp"
+
+class AnimationSpriteTimerTest : public ::testing::Test {
+   protected:
+    AnimationSpriteTimerTest() : timer(2000, 10) { SDL_Manager::initialize(); }
+    ~AnimationSpriteTimerTest() { SDL_Manager::quit(); }
+
+    Animation_Sprite_Timer timer;
+};
+
+TEST_F(AnimationSpriteTimerTest, Test1) {
+    Uint32 startTime = SDL_GetTicks();
+    timer.start_countdown();
+
+    EXPECT_EQ(0, timer.index_of_sprite_displayed_now());
+
+    while (SDL_GetTicks() - startTime < 200)
+        ;
+
+    EXPECT_EQ(1, timer.index_of_sprite_displayed_now());
+
+    while (SDL_GetTicks() - startTime < 600)
+        ;
+
+    EXPECT_EQ(3, timer.index_of_sprite_displayed_now());
+
+    while (SDL_GetTicks() - startTime < 1800)
+        ;
+
+    EXPECT_EQ(9, timer.index_of_sprite_displayed_now());
+}
