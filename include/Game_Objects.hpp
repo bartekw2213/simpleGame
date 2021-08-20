7ddb3751b9d985999e89a330f3548ad7b6e57114
bartekw2213 <bartekw2213@gmail.com>
2021-08-20 12:37:25 +0200

    Created initial declaration and definition of Game class

---
+++ include/Game_Objects.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "SDL_Objects.hpp"
+
+#define GAME_NAME "Flappy Bird"
+#define GAME_WIDTH 400
+#define GAME_HEIGHT 700
+#define TEXTURE_PATH "/usr/"
+
+class Game {
+   public:
+    Game();
+    ~Game();
+    void start();
+
+   private:
+    SDL_Window_Wrapper window_wrapper;
+    SDL_Renderer_Wrapper renderer_wrapper;
+    SDL_Texture_Wrapper texture_wrapper;
+};
