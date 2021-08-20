9d8bce9b7f5faf6f9e1c911ad3fc8b6a4a49cc52
bartekw2213 <bartekw2213@gmail.com>
2021-08-20 20:25:17 +0200

    Added SDL_Image initialization with PNG loading flag to SDL_Manager::initialize()

--- src/SDL_Objects/SDL_Manager.cpp
+++ src/SDL_Objects/SDL_Manager.cpp
@@ -2,7 +2,10 @@
 
 void SDL_Manager::initialize() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) SDL_Manager::throw_SDL_error();
-};
+
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) SDL_Manager::throw_IMG_error();
+}
 
 void SDL_Manager::quit() { SDL_Quit(); };
 
