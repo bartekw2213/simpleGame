755c13dead8afa97097cad675c563fcf1ba5a4ba
bartekw2213 <bartekw2213@gmail.com>
2021-08-20 12:38:44 +0200

    Main executable function only runs Game class method start()

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
-#include "SDL_Objects.hpp"
+#include "Game_Objects.hpp"
 
 int main(int argc, char* argv[]) {
-    SDL_Window_Wrapper window_wrapper;
-
-    SDL_Manager::initialize();
-
-    window_wrapper.initialize_window("Simple Game", 400, 400);
-
-    SDL_Manager::quit();
+    Game game;
+    game.start();
     return 0;
 }
