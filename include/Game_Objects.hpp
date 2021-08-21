cc8d848fcce32dee46995b1396f88ae3d826cd62
bartekw2213 <bartekw2213@gmail.com>
2021-08-21 11:48:48 +0200

    Changed TEXTURE_PATH constant to point to main sprite in resources dir

--- include/Game_Objects.hpp
+++ include/Game_Objects.hpp
@@ -4,7 +4,7 @@
 #define GAME_NAME "Flappy Bird"
 #define GAME_WIDTH 400
 #define GAME_HEIGHT 700
-#define TEXTURE_PATH "/usr/"
+#define TEXTURE_PATH "../resources/sprites/main-sprite.png"
 
 class Game {
    public:
