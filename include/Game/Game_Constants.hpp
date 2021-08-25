827ab9ba3ce250f325f1a5c8ab2b47706e9a2c2d
bartekw2213 <bartekw2213@gmail.com>
2021-08-25 11:08:00 +0200

    Added constants regarding game modes

--- include/Game/Game_Constants.hpp
+++ include/Game/Game_Constants.hpp
@@ -7,3 +7,17 @@
 #define GAME_WIDTH 400
 #define GAME_HEIGHT 700
 #define TEXTURE_PATH "../../resources/sprites/main-sprite.png"
+
+/*********************************
+ * Constants regarding game modes
+ *********************************/
+#define START_MENU_OBJECTS_MANAGER_NUM 1
+#define GAME_IN_PROGRESS_OBJECTS_MANAGER_NUM 0
+#define GAME_OVER_OBJECTS_MANAGER_NUM 0
+
+#define GAME_MODES_NUM 3
+
+/**********************************************
+ * Enum class representing possible game modes
+ **********************************************/
+enum class GAME_MODE { START_MENU, GAME_IN_PROGRESS, GAME_OVER };
